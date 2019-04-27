a2ccfbddaff1a28e01bd66db217be666bded2f7f
ChangJoo Park <pcjpcj2@gmail.com>
2019-04-27 21:00:35 +0900

    Add find_by_id_with_posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   # On when production ready
   # has_paper_trail on: [:update :destroy]
   has_many :posts
+
+  private
+    def self.find_by_id_with_posts(user_id)
+      self.includes(:posts).find(user_id)
+    end
 end
