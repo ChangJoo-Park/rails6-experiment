0dc603148467c8773d666b67041e7d8054d18a76
ChangJoo Park <pcjpcj2@gmail.com>
2019-04-27 20:58:57 +0900

    Set published_at when published true

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,19 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+
   has_rich_text :content
   belongs_to :user
   # On when production ready
   # has_paper_trail on: [:update :destroy]
+
+  before_save :set_published_at
+
+  private
+    def set_published_at
+      if self.published?
+        self.published_at = DateTime.now
+      else
+        self.published_at = nil
+      end
+    end
 end
