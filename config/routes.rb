04aeeec36b32b423d3e1eebae1cd75fcf7239245
ChangJoo Park <pcjpcj2@gmail.com>
2019-04-28 10:50:53 +0900

    Update route. devise_for :users must upon the 'user'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/:id', to: 'users#show', as: 'user'
   root 'posts#index'
   devise_for :users
   resources :posts
+  get 'users/:id', to: 'users#show', as: 'user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
