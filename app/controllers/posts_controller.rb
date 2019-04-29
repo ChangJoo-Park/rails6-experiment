9558c5e9060db21cae835e9dfdc6095a4e1215fc
ChangJoo Park <pcjpcj2@gmail.com>
2019-04-29 18:42:53 +0900

    Add tags routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.where({ published: true }).except(:content).includes(:user).order(published_at: :asc).limit(10)
+    params[:tag] ? @posts = Post.tagged_with(params[:tag]) : @posts = Post.where({ published: true }).except(:content).includes(:user).order(published_at: :asc).limit(10)
   end
 
   # GET /posts/1
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create, :update, :destory]
   end
+
+  get 'tags/:tag', to: 'posts#index', as: :tag
+
   get 'users/:id', to: 'users#show', as: 'user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
