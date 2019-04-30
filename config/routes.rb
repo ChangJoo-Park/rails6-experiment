c3fe184c873baeab4e2eb9ae5432b93d9a3b2981
ChangJoo Park <pcjpcj2@gmail.com>
2019-04-30 23:20:29 +0900

    Update user_profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'user_profile/edit'
-  get 'user_profile/update'
   root 'posts#index'
   devise_for :users
+
   resources :posts do
     resources :comments, only: [:create, :update, :destory]
   end
 
   get 'tags/:tag', to: 'posts#index', as: :tag
 
-  get 'users/:id', to: 'users#show', as: 'user'
+  resources :users, only: [:show] do
+    get 'profile', to: 'user_profile#edit'
+    resources :user_profile, only: [:update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
