2d62358898613970362913f8fb04095ae848350b
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-06 15:54:18 +0900

    Update feeds and tagged_with

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,12 +18,11 @@ class Post < ApplicationRecord
   # https://rubyplus.com/articles/4241-Tagging-from-Scratch-in-Rails-5
 
   def self.feeds
-    # FIXME: change when completed
     with_attached_cover
-    .except(:content)
-    .includes([:tags, :cover_attachment, user: [user_profile: :avatar_attachment]])
-    .order(created_at: :desc)
-    # .where({ published: true })
+      .except(:content)
+      .includes([:tags, :cover_attachment, user: [user_profile: :avatar_attachment]])
+      .order(created_at: :desc)
+      .where(published: true)
   end
 
   def self.all_by_user(user)
@@ -40,7 +39,7 @@ class Post < ApplicationRecord
   end
 
   def self.tagged_with(name)
-    Tag.find_by!(name: name).posts.limit(20).includes([:tags, :cover_attachment, user: [user_profile: :avatar_attachment]])
+    Tag.find_by!(name: name).posts.includes([:tags, :cover_attachment, user: [user_profile: :avatar_attachment]])
   end
 
   def self.tag_counts
