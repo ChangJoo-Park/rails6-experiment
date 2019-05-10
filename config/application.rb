919970302d44a2114584160d17e31c1bad41458e
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-10 23:07:41 +0900

    Update styles

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Rails6Experimental
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.i18n.default_locale = :ko
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
