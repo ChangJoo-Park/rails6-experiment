8f934a600cd5b5ee3b6f49d7cd8e9b5f750a6db3
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-12 22:58:09 +0900

    글 수정 스타일 추가

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 
 module ApplicationHelper
   def adorable_avatar(email, size = 285)
-    "<img src=\"https://api.adorable.io/avatars/285/#{email}\" class=\"img-fluid rounded\" width=\"#{size}px\">".html_safe
+    "<img src=\"https://api.adorable.io/avatars/285/#{email}\" class=\"img-fluid rounded\">".html_safe
   end
 end
