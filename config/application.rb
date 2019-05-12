62607f704af431cd7171a31d94c3374e8bee567d
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-12 23:06:21 +0900

    Set default timezone to Seoul

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Rails6Experimental
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ko
+    config.time_zone = "Seoul"
+    config.active_record.default_timezone = :utc
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
